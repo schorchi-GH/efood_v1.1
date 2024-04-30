cc8af29c0a3134459e8535af4cf190661f49f517
= <=>
2024-04-30 11:26:36 -0300

    fix: Concluído modal estético, falta implementar a dinamica de abrir e fechar

--- src/components/FoodList/index.tsx
+++ src/components/FoodList/index.tsx
@@ -1,29 +1,76 @@
 import FoodModel from '../../models/Food'
-import { List, Container } from './styles'
+import {
+  List,
+  Container,
+  CloseIcon,
+  ModalContent,
+  FoodDescription,
+  FoodImage,
+  FoodTitle,
+  Modal,
+  ModalContainer
+} from './styles'
 import Food from '../../components/Food'
+import pizza from '../../assets/pizza.png'
+import { AddCartButton } from '../Food/styles'
+import close from '../../assets/close 1.png'
+import { useState } from 'react'
 
 export type Props = {
   foods: FoodModel[]
 }
 
-const FoodList = ({ foods }: Props) => (
-  <>
-    <Container>
-      <List>
-        {foods.map((food) => (
-          <li key={food.id}>
-            <Food
-              key={food.id}
-              FoodPhoto={food.foodPhoto}
-              FoodTitle={food.foodTitle}
-              FoodDescription={food.foodDescription}
-              FoodPhotoAlt={food.foodPhotoAlt}
-            />
-          </li>
-        ))}
-      </List>
-    </Container>
-  </>
-)
+const foodSelected: FoodModel = {
+  id: 1,
+  foodTitle: 'Pizza Marquerita',
+  foodDescription:
+    'A pizza Margherita é uma pizza clássica da culinária italiana, reconhecida por sua simplicidade e sabores inigualável. Ela é fit com uma base de massa fina e crocante, coberta com molho de tomate fresco, queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva extra-virgem. A combinação de sabores é perfeita, com o molho de tomate suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma pizza simples, mas deliciosa, que agrada a todos os paladares e é uma ótima opção para qualquer ocasião.',
+  foodPhoto: pizza,
+  foodPhotoAlt: 'Pizza marguerita',
+  foodServe: 'de 2 a 3 pessoas',
+  foodPrice: 60.9
+}
+const FoodList = ({ foods }: Props) => {
+  const [showModal, setShowModal] = useState(true)
+  return (
+    <>
+      <Container>
+        <List>
+          {foods.map((food) => (
+            <li key={food.id}>
+              <Food
+                key={food.id}
+                FoodPhoto={food.foodPhoto}
+                FoodTitle={food.foodTitle}
+                FoodDescription={food.foodDescription}
+                FoodPhotoAlt={food.foodPhotoAlt}
+              />
+            </li>
+          ))}
+        </List>
+      </Container>
+      <Modal className={`${showModal}? "visible" : ""`}>
+        <ModalContent>
+          <FoodImage
+            src={foodSelected.foodPhoto}
+            alt={foodSelected.foodPhotoAlt}
+          />
+          <ModalContainer>
+            <FoodTitle>{foodSelected.foodTitle}</FoodTitle>
+            <FoodDescription>
+              {foodSelected.foodDescription}
+              <p>Serve: {foodSelected.foodServe}</p>
+            </FoodDescription>
+            <AddCartButton to={''}>
+              Adicionar ao carrinho - R$ {foodSelected.foodPrice}
+            </AddCartButton>
+          </ModalContainer>
+          <CloseIcon src={close} alt="Icone de fechar" />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
+  )
+}
 
 export default FoodList
--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -4,8 +4,6 @@ import FoodList from '../../components/FoodList'
 import Footer from '../../components/Footer'
 import FoodModel from '../../models/Food'
 import pizza from '../../assets/pizza.png'
-import ModalFood from '../../components/ModalFood'
-import Food from '../../models/Food'
 
 const foods: FoodModel[] = [
   {
@@ -70,23 +68,11 @@ const foods: FoodModel[] = [
   }
 ]
 
-const foodSelected: Food = {
-  id: 1,
-  foodTitle: 'Pizza Marquerita',
-  foodDescription:
-    'A pizza Margherita é uma pizza clássica da culinária italiana, reconhecida por sua simplicidade e sabores inigualável. Ela é fit com uma base de massa fina e crocante, coberta com molho de tomate fresco, queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva extra-virgem. A combinação de sabores é perfeita, com o molho de tomate suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma pizza simples, mas deliciosa, que agrada a todos os paladares e é uma ótima opção para qualquer ocasião.',
-  foodPhoto: pizza,
-  foodPhotoAlt: 'Pizza marguerita',
-  foodServe: 'de 2 a 3 pessoas',
-  foodPrice: 60.9
-}
-
 const Perfil = () => (
   <>
     <Header itens={0} />
     <Apresentacao />
     <FoodList foods={foods} />
-    <ModalFood foodModal={foodSelected} />
     <Footer />
   </>
 )
