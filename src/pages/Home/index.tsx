a281c9cdb7c0695915ab69ddc535d317fc594251
Schorchi <schorchi@gmail.com>
2024-05-22 18:28:34 -0300

    feat: acertar funcionalidade

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,36 +1,37 @@
-import Footer from '../../components/Footer'
-import Hero from '../../components/Hero'
-import RestaurantList from '../../components/RestaurantList'
-import { useGetRestaurantsQuery } from '../../services/api'
+import React from 'react';
+import Footer from '../../components/Footer';
+import Hero from '../../components/Hero';
+import RestaurantList from '../../components/RestaurantList';
+import { useGetRestaurantsQuery } from '../../services/api';
 
 export type Pedido = {
-  id: number
-  nome: string
-  foto: string
-  preco: number
-}
+  id: number;
+  nome: string;
+  foto: string;
+  preco: number;
+};
 export type Restaurant = {
-  id: number
-  titulo: string
-  destacado: boolean
-  tipo: string
-  avaliacao: number
-  descricao: string
-  capa: string
-  cardapio: [
-    {
-      foto: string
-      preco: number
-      id: number
-      nome: string
-      descricao: string
-      porcao: string
-    }
-  ]
-}
+  id: number;
+  titulo: string;
+  destacado: boolean;
+  tipo: string;
+  avaliacao: number;
+  descricao: string;
+  capa: string;
+  cardapio: {
+    foto: string;
+    preco: number;
+    id: number;
+    nome: string;
+    descricao: string;
+    porcao: string;
+  }[];
+};
 
 const Home = () => {
-  const { data: restaurants } = useGetRestaurantsQuery()
+  const { data: restaurants } = useGetRestaurantsQuery();
+
+  console.log('restaurants:', restaurants); // Add this line for debugging
 
   if (restaurants) {
     return (
@@ -39,8 +40,9 @@ const Home = () => {
         <RestaurantList restaurants={restaurants} />
         <Footer />
       </>
-    )
+    );
   }
-  return <h4>Loading...</h4>
-}
-export default Home
+  return <h4>Loading...</h4>;
+};
+
+export default Home;
